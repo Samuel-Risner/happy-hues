4dd4bf8ee00f4b45a8e1a962a40a0f8956aa6597
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-04-14 14:36:19 +0200

    Fixed logic and updated styling

--- src/pages/homePage.ts
+++ src/pages/homePage.ts
@@ -3,7 +3,7 @@ import CONSTANTS from "../constants";
 import { createHTMLelement } from "../createElement";
 import autoCreate from "../game/autoCreate";
 import LEVEL_DATA from "../levels/levelData";
-import type { T_GameGoal, T_PageHandler } from "../types";
+import type { T_BottleWidth, T_GameGoal, T_PageHandler } from "../types";
 import PageBase from "./pageBase";
 
 let closeLast: null | (() => void) = null;
@@ -73,136 +73,179 @@ function initLevelMode(parent: HTMLDivElement, pageHandler: T_PageHandler): void
 }
 
 function initCustom(parent: HTMLDivElement, pageHandler: T_PageHandler): void {
-    let gameGoal: T_GameGoal | null = null;
-    let width: number | null = null;
+    let gameGoal: T_GameGoal | null = CONSTANTS.CUSTOM_DEFAULTS.GAME_MODE;
+    let amountBottles: number | null = CONSTANTS.CUSTOM_DEFAULTS.AMOUNT_BOTTLES;
+    let width: T_BottleWidth | null = CONSTANTS.CUSTOM_DEFAULTS.BOTTLE_WIDTH;
+    let height: number | null = CONSTANTS.CUSTOM_DEFAULTS.HEIGHT;
+    let sameHeight: boolean = CONSTANTS.CUSTOM_DEFAULTS.SAME_HEIGHT;
 
     const menu = createDropdownMenu(parent, "Custom");
-    menu.className = "flex flex-col text-xl justify-center items-center";
-    
-    // game mode menu title
-    createHTMLelement("div", menu, { text: "Select game mode", className: "pb-4" });
+    menu.className = "flex flex-col grow text-lg justify-center items-center";
 
-    // game mode options
-    let gmButton: HTMLButtonElement | null = null;
+    function createGameModeMenu() {
+        // title
+        createHTMLelement("div", menu, { text: "Select game mode", className: "pb-4 text-xl" });
 
-    for (const gm of [null, ...CONSTANTS.SELECTABLE_GAME_GOALS]) {
-        const btn = createHTMLelement("button", menu, {
-            text: gm || "Random",
-            disabled: gm === null,
-            className: "text-lg text-anti-space/50 disabled:text-anti-space",
-            onclick: () => {
-                gameGoal = gm;
-                btn.disabled = true;
-                (gmButton as HTMLButtonElement).disabled = false;
-                gmButton = btn;
-            }
-        });
+        let lastClicked: HTMLButtonElement | null = null;
 
-        if (gm === null) gmButton = btn;
-    }
+        for (const mode of [null, ...CONSTANTS.SELECTABLE_GAME_GOALS]) {
+            const btn = createHTMLelement("button", menu, {
+                text: mode || "Random",
+                disabled: mode === gameGoal,
+                className: "text-anti-space/50 disabled:text-anti-space",
+                onclick: () => {
+                    gameGoal = mode;
+                    btn.disabled = true;
+                    (lastClicked as HTMLButtonElement).disabled = false;
+                    lastClicked = btn;
+                }
+            });
 
-    // height menu title
-    createHTMLelement("div", menu, { text: "Select bottle height", className: "py-4" });
-
-    const randomLabel = createHTMLelement("label", menu, { className: "flex flex-row justify-center items-center gap-2" });
-    const randomCheckbox = createHTMLelement("input", randomLabel, { checkbox: true, checked: true, onchange: (e) =>  {
-        if ((e.target as HTMLInputElement).checked) {
-            // console.log("checked");
-            heightSelectContainer.disabled = false;
-            sameHeightCheckbox.disabled = false;
-        } else {
-            // console.log("unchecked");
-            heightSelectContainer.disabled = true;
-            sameHeightCheckbox.disabled = true;
-            
+            if (mode === gameGoal) lastClicked = btn;
         }
-    }, className: "peer appearance-none w-4 h-4 bg-anti-space/50 checked:bg-anti-space rounded-sm" });
-    const randomText = createHTMLelement("div", randomLabel, { text: "Random", className: "text-anti-space/50 peer-checked:text-anti-space text-lg" });
-
-    const sameHeightLabel = createHTMLelement("label", menu, { className: "flex flex-row justify-center items-center gap-2" });
-    const sameHeightCheckbox = createHTMLelement("input", sameHeightLabel, { checkbox: true, onchange: (e) =>  {
-        if ((e.target as HTMLInputElement).checked) {
-            // console.log("checked");
-            randomCheckbox.checked = true;
-            heightSelectContainer.disabled = false;
-            sameHeightText.textContent = "Different height";
-        } else {
-            // console.log("unchecked");
-            // randomCheckbox.checked = false;
-            // heightSelectContainer.disabled = true;
-            sameHeightText.textContent = "Same height";
-        }
-    }, className: "peer appearance-none w-4 h-4 disabled:bg-anti-space/50 bg-anti-space rounded-sm" });
-    const sameHeightText = createHTMLelement("div", sameHeightLabel, { text: "Same height", className: "peer-disabled:text-anti-space/50 text-anti-space text-lg" });
-
-    let heightBtn: number = 5;
-    const allHeightBtns: HTMLButtonElement[] = [];
-
-    const heightSelectContainer = createHTMLelement("button", menu, { className: "grid grid-cols-15 gap-2 text-anti-space/50 disabled:text-anti-space", disabled: false, onclick: () => { 
-        randomCheckbox.checked = false;
-        sameHeightCheckbox.disabled = true;
-        heightSelectContainer.disabled = true;
-     } });
-
-    for (let i = CONSTANTS.EDIT.MIN_HEIGHT; i < 1 + CONSTANTS.EDIT.MAX_HEIGHT; i++) {
-        const btn = createHTMLelement("button", heightSelectContainer, {
-            text: `${i}`,
-            className: "border-2 border-transparent disabled:border-anti-space rounded-lg w-9 h-9 text-lg",
-            onclick: () => {
-                allHeightBtns[heightBtn - CONSTANTS.EDIT.MIN_HEIGHT].disabled = false;
-                heightBtn = i;
-                btn.disabled = true;
-                // console.log(i);
-            }
-        });
-        allHeightBtns.push(btn);
     }
 
-    // default height
-    allHeightBtns[heightBtn - CONSTANTS.EDIT.MIN_HEIGHT].disabled = true;
-
-    // height menu
-
-        // fixed
-
-        // from x to y
-
-        // random fixed
-
-        // random x to y
-
-        // completely random
-    
-    // width menu title
-    createHTMLelement("div", menu, { text: "Select bottle width", className: "py-4" });
-
-    // width options
-    let widthButton: HTMLButtonElement | null = null;
-    
-    for (const w of [null, ...CONSTANTS.EDIT.WIDTHS]) {
-        const btn = createHTMLelement("button", menu, {
-            text: w === null? "Random" : `width: ${w}`,
-            disabled: w === null,
-            className: "text-lg text-anti-space/50 disabled:text-anti-space",
-            onclick: () => {
-                width = w;
-                btn.disabled = true;
-                (widthButton as HTMLButtonElement).disabled = false;
-                widthButton = btn;
-            }
-        });
-
-        if (widthButton === null) widthButton = btn;
-    }
-    
     // difficulty
+
+    function createAmountBottlesMenu() {
+        // title
+        createHTMLelement("div", menu, { text: "Select amount bottles", className: "text-xl py-4" });
+
+        const allButtons: HTMLButtonElement[] = [];
+        
+        // random
+        const random = createHTMLelement("button", menu, {
+            text: "Random",
+            className: "text-anti-space/50 disabled:text-anti-space",
+            onclick: () => {
+                if (amountBottles !== null) allButtons[amountBottles - CONSTANTS.EDIT.BOTTLES_MIN].disabled = false;
+                random.disabled = true;
+                amountBottles = null;
+            }
+        });
+
+        // buttons for selecting amount
+
+        const btnContainer = createHTMLelement("div", menu, { className: "grid grid-cols-6 gap-1 justify-center items-center" });
+    
+        for (let i = CONSTANTS.EDIT.BOTTLES_MIN; i < 1 + CONSTANTS.EDIT.BOTTLES_MAX; i++) allButtons.push(createHTMLelement("button", btnContainer, {
+            text: `${i}`,
+            disabled: i === amountBottles,
+            className: "w-9 h-9 rounded-lg border-2 disabled:border-anti-space disabled:text-anti-space border-transparent text-anti-space/50",
+            onclick: () => {
+                if (amountBottles !== null) allButtons[amountBottles - CONSTANTS.EDIT.BOTTLES_MIN].disabled = false;
+                random.disabled = false;
+                allButtons[i - CONSTANTS.EDIT.BOTTLES_MIN].disabled = true;
+                amountBottles = i;
+            }
+        }));
+    }
+
+    function createWidthMenu() {
+        // title
+        createHTMLelement("div", menu, { text: "Select bottle width", className: "py-4 text-xl" });
+
+        let lastClicked: HTMLButtonElement | null = null;
+        
+        for (const w of [null, ...CONSTANTS.EDIT.WIDTHS]) {
+            const btn = createHTMLelement("button", menu, {
+                text: w === null? "Random" : `width: ${w}`,
+                disabled: w === width,
+                className: "text-anti-space/50 disabled:text-anti-space",
+                onclick: () => {
+                    width = w;
+                    btn.disabled = true;
+                    (lastClicked as HTMLButtonElement).disabled = false;
+                    lastClicked = btn;
+                }
+            });
+
+            if (lastClicked === width) lastClicked = btn;
+        }
+    }
+
+    function createHeightMenu() {
+        // title
+        createHTMLelement("div", menu, { text: "Select bottle height", className: "py-4 text-xl" });
+
+        // random
+
+        const randomLabel = createHTMLelement("label", menu, { className: "flex flex-row justify-center items-center gap-2" });
+        const randomCheckbox = createHTMLelement("input", randomLabel, { checkbox: true, checked: height === null, onchange: (e) =>  {
+            if ((e.target as HTMLInputElement).checked) {
+                // checked
+                heightSelectContainer.disabled = false;
+                sameHeightCheckbox.disabled = false;
+                if (height !== null) allButtons[height - CONSTANTS.EDIT.MIN_HEIGHT].disabled = false;
+                height = null;
+            } else {
+                // unchecked
+                heightSelectContainer.disabled = true;
+                sameHeightCheckbox.disabled = true;
+                
+            }
+        }, className: "peer appearance-none w-4 h-4 bg-anti-space/50 checked:bg-anti-space rounded-sm" });
+        createHTMLelement("div", randomLabel, { text: "Random", className: "text-anti-space/50 peer-checked:text-anti-space" }); // text
+
+        // same height
+        // checked: same height
+
+        const text_same = "Same height";
+        const text_different = "Different height";
+
+        const sameHeightLabel = createHTMLelement("label", menu, { className: "flex flex-row justify-center items-center gap-2" });
+        const sameHeightCheckbox = createHTMLelement("input", sameHeightLabel, { checkbox: true, disabled: height !== null, checked: sameHeight, onchange: (e) =>  {
+            if ((e.target as HTMLInputElement).checked) {
+                // checked
+                sameHeightText.textContent = text_same;
+                sameHeight = true;
+            } else {
+                // unchecked
+                sameHeightText.textContent = text_different;
+                sameHeight = false;
+            }
+        }, className: "peer appearance-none w-4 h-4 disabled:bg-anti-space/50 bg-anti-space rounded-sm" });
+        const sameHeightText = createHTMLelement("div", sameHeightLabel, { text: sameHeight? text_same : text_different, className: "peer-disabled:text-anti-space/50 text-anti-space" });
+
+        // specific heights
+        const allButtons: HTMLButtonElement[] = [];
+
+        const heightSelectContainer = createHTMLelement("button", menu, { className: "grid grid-cols-6 gap-1 text-anti-space/50 disabled:text-anti-space", disabled: height !== null, onclick: () => { 
+            randomCheckbox.checked = false;
+            sameHeightCheckbox.disabled = true;
+            heightSelectContainer.disabled = true;
+        } });
+
+        for (let i = CONSTANTS.EDIT.MIN_HEIGHT; i < 1 + CONSTANTS.EDIT.MAX_HEIGHT; i++) {
+            const btn = createHTMLelement("button", heightSelectContainer, {
+                text: `${i}`,
+                className: "border-2 border-transparent disabled:border-anti-space rounded-lg w-9 h-9",
+                onclick: () => {
+                    if (height !== null) allButtons[height - CONSTANTS.EDIT.MIN_HEIGHT].disabled = false;
+                    height = i;
+                    btn.disabled = true;
+                }
+            });
+
+            allButtons.push(btn);
+        }
+
+        // default height
+        if (height !== null) allButtons[height - CONSTANTS.EDIT.MIN_HEIGHT].disabled = true;
+    }
+
+    createGameModeMenu();
+    createAmountBottlesMenu();
+    createWidthMenu();
+    createHeightMenu();
 
     // start
     createHTMLelement("button", menu, {
         text: "Start",
-        className: "m-4 px-2 pb-2 pt-1 rounded-lg border-2",
+        className: "m-4 mt-8 px-2 pb-2 pt-1 rounded-lg border-2",
         onclick: () => {
-            pageHandler("GAME", { levelData: autoCreate(gameGoal, !(sameHeightCheckbox.checked && randomCheckbox.checked), heightBtn, width, null, null) });
+            pageHandler("GAME", { levelData: autoCreate(gameGoal, sameHeight, height, width, null, amountBottles) });
+            // console.log(`Game goal: ${gameGoal} Amount bottles: ${amountBottles} Width: ${width}, Height: ${height} Same height: ${sameHeight}`);
         }
     });
 }
@@ -229,7 +272,7 @@ export default class HomePage extends PageBase {
     constructor(parent: HTMLDivElement, pageHandler: T_PageHandler) {
         super(parent, pageHandler);
 
-        this.element.className = "flex grow flex-col p-4 gap-4 text-2xl";
+        this.element.className = "flex grow flex-col p-4 gap-4 text-2xl overflow-y-auto";
 
         initLevelMode(this.element, pageHandler);
         initCustom(this.element, pageHandler);
