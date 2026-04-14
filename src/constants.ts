48c35e9bf430d37cc13fc95ca2ea94c765c5e884
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-04-14 14:34:51 +0200

    Added default values and fixed types

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import type { T_ColorNames, T_ColorsTWCSS, T_GameGoal } from "./types";
+import type { T_BottleWidth, T_ColorNames, T_ColorsTWCSS, T_GameGoal } from "./types";
 
 const SIZES = {
     unit_px: 35
@@ -26,6 +26,23 @@ for (const colorName in COLORS_HEX) {
 
 const SELECTABLE_GAME_GOALS: T_GameGoal[] = ["sort-simple", "sort-exact"];
 
+// `null` -> random
+const CUSTOM_DEFAULTS: {
+    GAME_MODE: T_GameGoal | null,
+    AMOUNT_BOTTLES: number | null,
+    BOTTLE_WIDTH: 1 | null,
+    HEIGHT: number | null,
+    SAME_HEIGHT: boolean,
+} = {
+    GAME_MODE: "sort-simple",
+    AMOUNT_BOTTLES: 7,
+    BOTTLE_WIDTH: 1,
+    HEIGHT: 5,
+    SAME_HEIGHT: true,
+}
+
+const WIDTHS: T_BottleWidth[] = [1];
+
 const CONSTANTS = {
     SIZES: SIZES,
 
@@ -35,7 +52,7 @@ const CONSTANTS = {
     COLOR_TO_REPLACE_HEX: "00ffff",
 
     EDIT: {
-        WIDTHS: [1],
+        WIDTHS: WIDTHS,
         MIN_HEIGHT: 3,
         MAX_HEIGHT: 20,
         BOTTLES_MIN: 3,
@@ -43,6 +60,8 @@ const CONSTANTS = {
     },
 
     SELECTABLE_GAME_GOALS: SELECTABLE_GAME_GOALS,
+
+    CUSTOM_DEFAULTS: CUSTOM_DEFAULTS,
 }
 
 export default CONSTANTS;
