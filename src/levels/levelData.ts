3ddfdbd2bcfcebf8a7a7599fb4e21c35fb3fbc11
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2026-06-08 18:54:05 +0200

    Added level 3

--- src/levels/levelData.ts
+++ src/levels/levelData.ts
@@ -2,9 +2,10 @@ import type { T_GameJSON } from "../types";
 
 import LEVEL_1_1 from "./1/level-1-1.json";
 import LEVEL_1_2 from "./1/level-1-2.json";
+import LEVEL_1_3 from "./1/level-1-3.json";
 
 const LEVEL_DATA: T_GameJSON[][] = [
-    [LEVEL_1_1 as T_GameJSON, LEVEL_1_2 as T_GameJSON]
+    [LEVEL_1_1 as T_GameJSON, LEVEL_1_2 as T_GameJSON, LEVEL_1_3 as T_GameJSON]
 ]
 
 export default LEVEL_DATA;
